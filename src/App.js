777ad47e441897e8d2ff0ee9082b3ea062f14247
dongthach <thachdong270293@gmail.com>
2021-10-15 09:29:57 +0700

    update some stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 import "./index.css";
 import Zoom from "./Zoom";
 
 export default function App() {
   const [joinMeeting, setJoinMeeting] = useState(false);
+  const [meetingNumber, setMeetingNumber] = useState();
 
-  if (joinMeeting) return <Zoom />
+  if (joinMeeting) {
+    return <Zoom meetingNumber={meetingNumber} />;
+  }
+
+  function handleJoinMeeting() {
+    if (!meetingNumber) {
+      alert("Enter Meeting Number before join meeting !");
+      return;
+    }
+    setJoinMeeting(true);
+  }
 
   return (
     <div className="app">
-      <button className="join-btn" onClick={() => setJoinMeeting(true)}>
-        Join meeting
-      </button>
+      <div>
+        <div>
+          <input
+            type="number"
+            className="meeting-number"
+            placeholder="Enter Meeting Number"
+            value={meetingNumber}
+            onChange={(e) => setMeetingNumber(e.target.value)}
+          />
+        </div>
+
+        <button className="join-btn" onClick={handleJoinMeeting}>
+          Join Meeting
+        </button>
+      </div>
     </div>
-  )
+  );
 }
--- src/Zoom.js
+++ src/Zoom.js
@@ -1,46 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ZoomMtg } from "@zoomus/websdk";
 
 const crypto = require("crypto"); // crypto comes with Node.js
 
-function generateSignature(apiKey, apiSecret, meetingNumber, role) {
-  return new Promise((res, rej) => {
-    const timestamp = new Date().getTime() - 30000;
-    const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
-      "base64"
-    );
-    const hash = crypto
-      .createHmac("sha256", apiSecret)
-      .update(msg)
-      .digest("base64");
-    const signature = Buffer.from(
-      `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
-    ).toString("base64");
+export default function Zoom({meetingNumber}) {
+  function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+    return new Promise((res, rej) => {
+      const timestamp = new Date().getTime() - 30000;
+      const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
+        "base64"
+      );
+      const hash = crypto
+        .createHmac("sha256", apiSecret)
+        .update(msg)
+        .digest("base64");
+      const signature = Buffer.from(
+        `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
+      ).toString("base64");
+  
+      res(signature);
+    });
+  }
+  
+  var apiKey = "7jgR7KkeSJGCAYlgcuDFCQ";
+  var apiSecret = "u0nvB6m5wbLwl0SeE5sUWXFIyhs564xo1hTq";
+  // var meetingNumber = 98058213775;
+  var leaveUrl = "http://localhost:3000";
+  var userName = "WebSDK";
+  var userEmail = "";
+  var passWord = "";
+  var signature = "";
 
-    res(signature);
-  });
-}
+  generateSignature(apiKey, apiSecret, meetingNumber, 0).then(
+    (res) => (signature = res)
+  );
 
-var apiKey = "7jgR7KkeSJGCAYlgcuDFCQ";
-var apiSecret = "u0nvB6m5wbLwl0SeE5sUWXFIyhs564xo1hTq";
-var meetingNumber = 98058213775;
-var leaveUrl = "http://localhost:3000";
-var userName = "WebSDK";
-var userEmail = "";
-var passWord = "";
-var signature = "";
-generateSignature(apiKey, apiSecret, meetingNumber, 0).then(
-  (res) => (signature = res)
-);
-
-export default function Zoom() {
   useEffect(() => {
     showZoomDiv();
     ZoomMtg.setZoomJSLib("https://source.zoom.us/1.9.0/lib", "/av");
     ZoomMtg.preLoadWasm();
     ZoomMtg.prepareJssdk();
     initiateMeeting();
-  }, []);
+  }, [meetingNumber]);
 
   const showZoomDiv = () => {
     document.getElementById("zmmtg-root").style.display = "block";
